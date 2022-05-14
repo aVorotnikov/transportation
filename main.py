4eff8182602f78bc687698784691a74c3f25482f
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-05-14 15:19:20 +0300

    Add north-west method

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from get_task import get_task
+from north_west import generate_initial
 
-
-a, b, c = get_task("task.txt")
-print("Need: ", a)
-print("Suppliers: ", b)
-print("Costs: \n", c)
+try:
+    a, b, c = get_task("task.txt")
+    print("Need: ", a)
+    print("Suppliers: ", b)
+    print("Costs:\n", c)
+    x_initial = generate_initial(a, b)
+    print("Initial matrix:\n", x_initial)
+except Exception as error:
+    print(error)
---
+++ north_west.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def generate_initial(a, b):
+    x = np.zeros((len(a), len(b)))
+    b0 = np.copy(b)
+    for need in range(0, len(a)):
+        val = a[need]
+        for sup in range(0, len(b0)):
+            if b0[sup] < val:
+                x[need][sup] = b0[sup]
+                val -= b0[sup]
+                b0[sup] = 0
+            else:
+                x[need][sup] = val
+                b0[sup] -= val
+                val = 0
+        if val != 0:
+            raise Exception("Task cannot be solved")
+    return x
