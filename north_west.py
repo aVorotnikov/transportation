da530fb09b839c4a5de4519169adc994d7481265
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-05-14 15:34:45 +0300

    Add potential method pipeline

--- north_west.py
+++ north_west.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def generate_initial(a, b):
     x = np.zeros((len(a), len(b)))
     b0 = np.copy(b)
---
+++ potential.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def optimize(x_initial, a, b, c):
+    x = np.copy(x_initial)
+    return x
