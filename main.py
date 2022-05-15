82c8dd4eabeccf33067eda966125937bf7b7a344
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-05-15 19:15:18 +0300

    Add target function evaluating

--- main.py
+++ main.py
@@ -24,12 +24,23 @@ try:
     print("Initial matrix:\n", x_initial)
     x = optimize(x_initial, a, b, c)
     print("Result:\n", x)
+    sum = 0
+    for i in range(0, len(x)):
+        for j in range(0, len(x[i])):
+            sum += x.item(i, j) * c.item(i, j)
+    print("Total: ", sum)
     print("BRUTE-FORCE")
     c_canon, a_canon, b_canon = make_canon(a, b, c)
     a_canon.pop(len(a_canon) - 1)
     b_canon.pop(len(b_canon) - 1)
     sol, function = solve_brute_force(a_canon, b_canon, c_canon)
+    print("Result:\n", x)
     sol = np.reshape(sol, (len(a), len(b)))
     print(sol)
+    sum = 0
+    for i in range(0, len(x)):
+        for j in range(0, len(x[i])):
+            sum += x.item(i, j) * c.item(i, j)
+    print("Total: ", sum)
 except Exception as error:
     print(error)
