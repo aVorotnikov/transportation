c305372835fd20033447ca3f39db0f4879e70a4a
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-05-15 13:01:07 +0300

    Add potentials evluating

--- potential.py
+++ potential.py
@@ -2,6 +2,33 @@ import numpy as np
 import copy
 
 
+def get_potentials(x, a, b, c):
+    u = np.zeros(len(a))
+    v = np.zeros(len(b))
+
+    A = [[0 for j in range(len(b) + len(a))] for i in range(len(b) + len(a))]
+    b0 = [0 for i in range(len(b) + len(a))]
+
+    A[0][0] = 1
+    b0[0] = 0
+    var = 1
+    for i in range(0, len(a)):
+        for j in range(0, len(b)):
+            if x[i][j] is not None:
+                A[var][i] = -1
+                A[var][len(a) + j] = 1
+                b0[var] = c.item(i, j)
+                var = var + 1
+    x = np.linalg.solve(A, b0)
+    for i in range(len(a) + len(b)):
+        if i < len(a):
+            u[i] = x[i]
+        else:
+            v[i - len(a)] = x[i]
+
+    return u, v
+
+
 def check_end(x, c, u, v):
     for i in range(0, len(x)):
         for j in range(0, len(x[i])):
@@ -24,8 +51,7 @@ def finalize(x):
 
 def optimize(x_initial, a, b, c):
     x = copy.deepcopy(x_initial)
-    u = np.zeros(len(a))
-    v = np.zeros(len(b))
+    u, v = get_potentials(x, a, b, c)
     while not check_end(x, c, u, v):
         pass
     return finalize(x)
